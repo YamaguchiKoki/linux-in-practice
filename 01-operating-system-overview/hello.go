cba2b880096bd3ddb2c553dc41635beb71cc3dcb
YamaguchiKoki <shankouhanghui15@gmail.com>
2025-04-13 14:42:45 +0900

    システムコール呼び出しまでを視覚化

--- 01-operating-system-overview/hello.go
+++ 01-operating-system-overview/hello.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"os"
+	"runtime/trace"
 )
 
-func main() {
+func _main() {
+	_, task := trace.NewTask(context.Background(), "main")
+	defer task.End()
+
 	fmt.Println("hello world")
+}
+
+func main() {
+	f, err := os.Create("tseq.out")
+	if err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalln("Error:", err)
+		}
+	}()
+
+	if err := trace.Start(f); err != nil {
+		log.Fatalln("Error:", err)
+	}
+	defer trace.Stop()
+
+	_main()
 }
